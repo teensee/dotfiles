9c521018a7b01408dc1600f87ea1a298e5d5ceca
vlad <stimulmonk@yandex.ru>
2026-04-03 22:37:17 +0300

    change treesitter main branch, upd config to 0.12
    upd lazy.lock

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,29 +1,29 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
+    branch = "main",
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+        local ts = require "nvim-treesitter"
+        ts.setup()
+
+        local parsers = {
             "vim",
             "lua",
             "vimdoc",
-
             -- Web
             "html",
             "css",
             "twig",
-
             -- Main languages
             "php",
             "phpdoc",
-
             "go",
             "gomod",
             "gosum",
             "gowork",
-
             "python",
-
             "sql",
-
             -- Config languages
             "yaml",
             "markdown",
@@ -32,15 +32,43 @@ return {
             "json",
             "http",
             "dockerfile",
-        },
-        auto_install = true, -- Автоматически устанавливать недостающие парсеры
-        sync_install = false, -- Синхронизация парсеров вручную, чтобы избежать подвисаний
-        highlight = {
-            enable = true, -- Включение подсветки кода
-            additional_vim_regex_highlighting = false,
-        },
-        indent = {
-            enable = true, -- Улучшенная автоматическая табуляция
-        },
-    },
+        }
+
+        ts.install(parsers)
+
+        -- Аналог auto_install — ставим парсер при открытии файла
+        -- Паттерны для автокоманды (filetype != parser name в некоторых случаях)
+        local ft_patterns = {
+            "vim",
+            "lua",
+            "help",
+            "html",
+            "css",
+            "twig",
+            "php",
+            "go",
+            "gomod",
+            "gosum",
+            "gowork",
+            "python",
+            "sql",
+            "yaml",
+            "markdown",
+            "bash",
+            "toml",
+            "json",
+            "http",
+            "dockerfile",
+        }
+
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = ft_patterns,
+            callback = function(args)
+                -- Включение подсветки (аналог highlight.enable = true)
+                vim.treesitter.start(args.buf)
+                -- Включение отступов (аналог indent.enable = true)
+                vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
 }
