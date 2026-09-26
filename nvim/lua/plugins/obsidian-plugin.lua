e5ee10f0743bf52386be7d56d4cceb21bc877c8b
vlad <stimulmonk@yandex.ru>
2026-09-26 18:29:44 +0300

    chore(nvim): drop deprecated obsidian.nvim options
    - remove completion.nvim_cmp/completion.blink: completion is provided by the
      built-in obsidian-ls LSP server, already covered by blink.cmp lsp source
    - set legacy_commands = false; mappings already use :Obsidian <subcommand>

--- nvim/lua/plugins/obsidian-plugin.lua
+++ nvim/lua/plugins/obsidian-plugin.lua
@@ -13,11 +13,13 @@ return {
                 path = vim.fn.expand "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ObsidianNotes",
             },
         },
+        -- completion идёт через встроенный obsidian-ls LSP (источник "lsp" в blink.cmp),
+        -- отдельные флаги nvim_cmp/blink удалены в 3.x и убираются в 4.0
         completion = {
-            nvim_cmp = false,
-            blink = true,
             min_chars = 2,
         },
+        -- старые команды (:ObsidianBacklinks) отключены, используем :Obsidian <subcommand>
+        legacy_commands = false,
         daily_notes = {
             folder = "Daily", -- Папка для ежедневных заметок
             date_format = "%Y-%m-%d", -- Формат даты для удобства
